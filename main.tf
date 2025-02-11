2392fe3b73728b328ae625ba053f798d726ff10a
dimilidi <dimitrovalidia@yahoo.com>
2025-02-11 13:39:09 +0100

    add backend resource

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.18.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragelidi"
+    container_name       = "taskboardstoragelidi"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
